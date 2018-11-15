804c30753b033c30b5bd7fecdda7bc2dd6262384
Łukasz Magiera <magik6k@gmail.com>
2018-11-15 03:16:47 +0100

    Fix jfs mkfs

--- options/options.go
+++ options/options.go
@@ -34,7 +34,7 @@ func (opt BenchOptions) TestDesc() string {
 
 var DefaultBenchOpts = OptionsRange2pow(
 	BenchOptions{1, 25 << 10, 64},
-	BenchOptions{1 << 5, 25 << 10, 64}, 2)
+	BenchOptions{1 << 5, 25 << 10, 64}, 4)
 
   // 20 / 25 / 13 is 1M / ~20G
 
